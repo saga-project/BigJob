1209703f050aa7e882749ea760a3398609bddd14
Andre Luckow <222015+drelu@users.noreply.github.com>
2011-09-15 19:52:57 +0000

    escaping of bootstrap script if used with Globus adaptor

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except:
     sys.exit(1)
     
 setup(name='BigJob',
-      version='0.2.8',
+      version='0.2.11',
       description='SAGA-based Pilot-Job Implementation',
       author='Andre Luckow',
       author_email='aluckow@cct.lsu.edu',
