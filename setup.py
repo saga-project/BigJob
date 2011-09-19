0790feca98f54e7bf79b44d3cfa51bc315af97b1
Andre Luckow <222015+drelu@users.noreply.github.com>
2011-09-19 20:06:19 +0000

    fixed indentation issue on some strange behaving systems (philip)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except:
     sys.exit(1)
     
 setup(name='BigJob',
-      version='0.2.72',
+      version='0.2.74',
       description='SAGA-based Pilot-Job Implementation',
       author='Andre Luckow',
       author_email='aluckow@cct.lsu.edu',
