cacf345aeda88c5045a50ec88acf60cc690d542f
Sharath Maddineni <smaddineni@cct.lsu.edu>
2011-09-26 22:46:17 +0000

    minor fix in condor bigjob agent to pwd into environment, changing version number to 0.3.03

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except:
     #sys.exit(1)
     
 setup(name='BigJob',
-      version='0.3.02',
+      version='0.3.03',
       description='SAGA-based Pilot-Job Implementation',
       author='Andre Luckow',
       author_email='aluckow@cct.lsu.edu',
