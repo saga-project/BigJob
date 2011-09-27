9b826434d65849cceb056ff4261f5c323da3029e
Sharath Maddineni <smaddineni@cct.lsu.edu>
2011-09-27 03:25:32 +0000

    another approach to run the executble in pwd, changing version no. to 0.3.04

--- bigjob/bigjob_agent_condor.py
+++ bigjob/bigjob_agent_condor.py
@@ -287,7 +287,7 @@ class bigjob_agent:
                     #if (host != socket.gethostname()):
                     #    command ="ssh  " + host + " \"cd " + workingdirectory + "; " + command +"\""     
                 else:
-                    command ="ssh  " + host + " \"cd " + workingdirectory + "; export PATH=$PATH:" + workingdirectory + ";" +command +"\""     
+                    command ="ssh  " + host + " 'cd " + workingdirectory + "; export PATH=$PATH:" + workingdirectory + ";" +workingdirectory+"/" +command +"'"     
                 shell = self.SHELL 
                 logging.debug("execute: " + command + " in " + workingdirectory + " from: " + str(socket.gethostname()) + " (Shell: " + shell +")")
                 # bash works fine for launching on QB but fails for Abe :-(
