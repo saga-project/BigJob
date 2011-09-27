0f99f8419798d324dc09cd2c28bff4dc6dd1e659
Sharath Maddineni <smaddineni@cct.lsu.edu>
2011-09-27 03:25:32 +0000

    another approach to run the executble in pwd, changing version no. to 0.3.04

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except:
     #sys.exit(1)
     
 setup(name='BigJob',
-      version='0.3.03',
+      version='0.3.04',
       description='SAGA-based Pilot-Job Implementation',
       author='Andre Luckow',
       author_email='aluckow@cct.lsu.edu',
