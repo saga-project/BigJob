5477f5e241171a5763375a802d8f5f76bbc9de2e
Andre Luckow <222015+drelu@users.noreply.github.com>
2011-10-22 09:40:24 +0000

    Updated examples: - removed absolute paths - make sure that they work out-of-the-box with SAGA Job Local adaptor and SAGA/Advert/SQLite

--- examples/example_local_multiple.py
+++ examples/example_local_multiple.py
@@ -1,4 +1,8 @@
 """ Example application demonstrating job submission via bigjob 
+
+    DON'T EDIT THIS FILE (UNLESS THERE IS A BUG)
+    
+    THIS FILE SHOULD NOT BE COMMITTED TO SVN WITH USE-SPECIFIC PATHS!
 """
 
 import saga
@@ -24,6 +28,7 @@ sys.path.insert(0, os.getcwd() + "/../")
 ### EDIT COORDINATION_URL to point to advert server.  
 #COORDINATION_URL = "advert://advert.cct.lsu.edu:5432/"
 COORDINATION_URL = "advert://advert.cct.lsu.edu:8080/"
+COORDINATION_URL = "advert://localhost/"
 
 from bigjob.bigjob_manager import bigjob, subjob
 
@@ -51,17 +56,25 @@ if __name__ == "__main__":
     walltime=100
     processes_per_node=4
     number_of_processes =2
-    workingdirectory="/home/pmantha/examples/agent"  # working directory for agent
+    workingdirectory= os.path.join(os.getcwd(), "agent") # working directory for agent
     userproxy = None # userproxy (not supported yet due to context issue w/ SAGA)
 
-    #lrms_url = "fork://localhost" # resource url to run the jobs on localhost
-    #lrms_url = "gram://oliver1.loni.org/jobmanager-pbs" # globus resource url used when globus is used. (LONI)
-    #lrms_url = "pbspro://louie1.loni.org" # pbspro resource url used when pbspro scheduling system is used.(Futuregrid or LSU Machines)
-    #lrms_url = "ssh://louie1.loni.org" # ssh resource url which launches jobs on target machine. Jobs not submitted to scheduling system.
+
+    """ 
+    URL of the SAGA Job Service that is used to dispatch the pilot job.
+    The following URLs are accepted:
+    
+    lrms_url = "gram://oliver1.loni.org/jobmanager-pbs" # globus resource url used when globus is used. (LONI)
+    lrms_url = "pbspro://louie1.loni.org" # pbspro resource url used when pbspro scheduling system is used.(Futuregrid or LSU Machines)
+    lrms_url = "ssh://louie1.loni.org" # ssh resource url which launches jobs on target machine. Jobs not submitted to scheduling system.
     lrms_url = "pbs-ssh://louie1.loni.org" # Submit jobs to scheduling system of remote machine.
-
-    #lrms_url = "xt5torque://localhost" # torque resource url 
-
+    lrms_url = "xt5torque://localhost" # torque resource url.
+    lrms_url = "xt5torque://localhost" # torque resource url    
+    
+    Please ensure that the respective SAGA adaptor is installed and working
+    """
+    lrms_url = "fork://localhost" 
+    
     ##########################################################################################
 
     print "Start Pilot Job/BigJob at: " + lrms_url
@@ -89,9 +102,9 @@ if __name__ == "__main__":
         jd.number_of_processes = "1"
         jd.spmd_variation = "single"
         jd.arguments = [""]
-        jd.working_directory = "/home/pmantha/examples/agent" 
-        jd.output = "/home/pmantha/examples/agent/sj-stdout-"+str(i)+".txt"
-        jd.error = "/home/pmantha/examples/agent/sj-stderr-"+str(i)+".txt"
+        jd.working_directory = os.getcwd() 
+        jd.output = os.path.join(os.getcwd(), "agent/sj-stdout-"+str(i)+".txt")
+        jd.error = os.path.join(os.getcwd(), "agent/sj-stderr-"+str(i)+".txt")
 
         sj = subjob()
         sj.submit_job(bj.pilot_url, jd)
@@ -106,6 +119,7 @@ if __name__ == "__main__":
         for i in range(0, NUMBER_JOBS):
             old_state = job_states[jobs[i]]
             state = jobs[i].get_state()
+            #print "Job " + str(jobs[i]) + " state: " + state
             if result_map.has_key(state)==False:
                 result_map[state]=1
             else:
--- examples/example_local_single.py
+++ examples/example_local_single.py
@@ -1,4 +1,8 @@
 """ Example application demonstrating job submission via bigjob 
+
+    DON'T EDIT THIS FILE (UNLESS THERE IS A BUG)
+    
+    THIS FILE SHOULD NOT BE COMMITTED TO SVN WITH USE-SPECIFIC PATHS!
 """
 
 import saga
@@ -22,8 +26,8 @@ import sys
         tcp://* (ZMQ - listening to all interfaces)
 """
 
-#COORDINATION_URL = "advert://localhost/"
-COORDINATION_URL = "advert://advert.cct.lsu.edu:8080/"
+COORDINATION_URL = "advert://localhost/"
+#COORDINATION_URL = "advert://advert.cct.lsu.edu:8080/"
 #COORDINATION_URL = "tcp://*"
 #COORDINATION_URL = "redis://localhost:6379"
 
@@ -42,16 +46,24 @@ def main():
     walltime=10
     processes_per_node=4
     number_of_processes = 8
-    workingdirectory=os.getcwd() +"/agent"  # working directory for agent
+    workingdirectory=os.path.join(os.getcwd(), "agent")  # working directory for agent
     userproxy = None # userproxy (not supported yet due to context issue w/ SAGA)
 
-    #lrms_url = "fork://localhost" # resource url to run the jobs on localhost
-    #lrms_url = "gram://oliver1.loni.org/jobmanager-pbs" # globus resource url used when globus is used. (LONI)
-    #lrms_url = "pbspro://louie1.loni.org" # pbspro resource url used when pbspro scheduling system is used.(Futuregrid or LSU Machines)
-    #lrms_url = "ssh://louie1.loni.org" # ssh resource url which launches jobs on target machine. Jobs not submitted to scheduling system.
+    
+    """ 
+    URL of the SAGA Job Service that is used to dispatch the pilot job.
+    The following URLs are accepted:
+    
+    lrms_url = "gram://oliver1.loni.org/jobmanager-pbs" # globus resource url used when globus is used. (LONI)
+    lrms_url = "pbspro://louie1.loni.org" # pbspro resource url used when pbspro scheduling system is used.(Futuregrid or LSU Machines)
+    lrms_url = "ssh://louie1.loni.org" # ssh resource url which launches jobs on target machine. Jobs not submitted to scheduling system.
     lrms_url = "pbs-ssh://louie1.loni.org" # Submit jobs to scheduling system of remote machine.
-    #lrms_url = "xt5torque://localhost" # torque resource url.
- 
+    lrms_url = "xt5torque://localhost" # torque resource url.
+    
+    Please ensure that the respective SAGA adaptor is installed and working
+    """
+    lrms_url = "fork://localhost" # resource url to run the jobs on localhost
+   
     ##########################################################################################
 
     print "Start Pilot Job/BigJob at: " + lrms_url
