9a6e765c5289d6ac510ff7b8fa3c0ef3eee6a777
Ole Weidner <ole.weidner@me.com>
2012-01-13 11:15:26 -0500

    towards an OSG 'bigjob'

---
+++ bigjob_osg/subjob_impl.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+__author__    = "Ole Christian Weidner"
+__copyright__ = "Copyright 2012, Ole Christian Weidner"
+__license__   = "MIT"
+
+class osg_subjob(object):
+    
+    def __init__(self, database_host):
+        pass
+    
+    def submit_job(self, pilot_url, jd):
+        pass
+    
+    def get_state(self):        
+        pass
+    
+    def cancel(self):
+        pass
+
