3a4f74a94f1625da8f1281f0222648f8a809890b
Andre Luckow <andre.luckow@googlemail.com>
2012-02-05 19:07:27 +0100

    made logging level configurable via bigjob.conf

--- bigjob/__init__.py
+++ bigjob/__init__.py
@@ -1,17 +1,7 @@
 import os
 import logging
-logging.basicConfig(level=logging.DEBUG, datefmt='%m/%d/%Y %I:%M:%S %p',
-                   format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(name='bigjob')
 
 version = "latest"
-
-try:
-    fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", 'VERSION')
-    version = open(fn).read().strip()
-    logger.debug("Loading BigJob version: " + version)
-except IOError:
-    pass
 
 #READ config
 SAGA_BLISS=False
@@ -20,15 +10,37 @@ try:
     _CONFIG_FILE="bigjob.conf"
     _conf_file = os.path.dirname(os.path.abspath( __file__ )) + "/../" + _CONFIG_FILE
     _config = ConfigParser.ConfigParser()
-    logger.debug ("read configfile: " + _conf_file)
     _config.read(_conf_file)
-    default_dict = _config.defaults()        
+    default_dict = _config.defaults()
+    
+    level = default_dict["logging.level"]
+    if level.startswith("logging."):
+        logging_level = eval(level)        
+        print("Set logging level: %s/%s"%(level,logging_level))
+        logging.basicConfig(level=logging_level, datefmt='%m/%d/%Y %I:%M:%S %p',
+                   format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    else:
+        logging.basicConfig(level=logging.DEBUG, datefmt='%m/%d/%Y %I:%M:%S %p',
+                   format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')    
+        
+    logger = logging.getLogger(name='bigjob')
+            
     saga = default_dict["saga"]
-
     if saga.lower() == "bliss":
         SAGA_BLISS=True    
+        
 except:
-    logger.error("bjgjob.conf could not be read") 
+    print("bjgjob.conf could not be read") 
+    
+
+try:
+    fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", 'VERSION')
+    version = open(fn).read().strip()
+    logger.info("Loading BigJob version: " + version)
+except IOError:
+    pass
+
+
 
 # define external-facing API
 from bigjob.bigjob_manager import bigjob as myBigjob
