0f471bebbbb9469f75f6599bde28a7bba1f3aed5
Pradeep Kumar Pradeep Kumar <pmantha@s1.(none)>
2012-04-04 16:31:48 -0700

    minor change

--- bigjob/bigjob_agent.py
+++ bigjob/bigjob_agent.py
@@ -377,7 +377,8 @@ class bigjob_agent:
                     command =  envi + executable + " " + arguments
                 else:
                     # In particular for Condor - if executable is staged x flag is not set
-                    command ="chmod +x " + executable +";export PATH=$PATH:" + workingdirectory + ";" +command
+                    #command ="chmod +x " + executable +";export PATH=$PATH:" + workingdirectory + ";" +command
+                    command ="chmod +x " + executable +";export PATH=$PATH:" + workingdirectory + ";" + arguments
                     # command =  executable + " " + arguments
                 #pdb.set_trace()
                 # special setup for MPI NAMD jobs
