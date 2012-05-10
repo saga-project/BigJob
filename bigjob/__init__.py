fd2638675ad46d23bf0a1e2995a6df5f0259f543
Andre Luckow <andre.luckow@googlemail.com>
2012-05-10 22:21:53 +0200

    Support for managing the logging level via BIGJOB_VERBOSE environment variable

--- bigjob/__init__.py
+++ bigjob/__init__.py
@@ -1,6 +1,7 @@
 import os
+import sys
 import logging
-
+import traceback
 version = "latest"
 
 #READ config
@@ -13,28 +14,53 @@ try:
     _config.read(_conf_file)
     default_dict = _config.defaults()
     
-    level = default_dict["logging.level"]
-    print("Logging level: %s"%level) 
-    if level.startswith("logging."):
-        logging_level = eval(level)        
-        print("Set logging level: %s/%s"%(level,logging_level))
-        logging.basicConfig(level=logging.INFO, datefmt='%m/%d/%Y %I:%M:%S %p',
-                   format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        logger = logging.getLogger(name='bigjob')
-        logger.setLevel(logging_level)
-    
+    ####################################################
+    # logging
+    logging_level = logging.FATAL
+    BIGJOB_VERBOSE=None
+    try: 
+        BIGJOB_VERBOSE = int(os.getenv('BIGJOB_VERBOSE'))
+        #print("BIGJOB_VERBOSE: %d"%BIGJOB_VERBOSE)
+    except Exception:
+        pass   
+     
+    if BIGJOB_VERBOSE==None: # use logging level defined in config file
+        print "Read log level from bigjob.conf"
+        level = default_dict["logging.level"]
+        print("Logging level: %s"%level) 
+        if level.startswith("logging."):
+            logging_level = eval(level)     
     else:
-        logging.basicConfig(level=logging.INFO, datefmt='%m/%d/%Y %I:%M:%S %p',
-                   format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')    
-        logger = logging.getLogger(name='bigjob')
-            
+        # 4 = DEBUG + INFO + WARNING + ERROR
+        if BIGJOB_VERBOSE >= 4:
+            print "set to DEBUG"
+            logging_level = logging.DEBUG
+        # 3 = INFO + WARNING + ERROR
+        elif BIGJOB_VERBOSE == 3:
+            logging_level = logging.INFO
+        # 2 = WARNING + ERROR 
+        elif BIGJOB_VERBOSE == 2:
+            logging_level = logging.WARNING
+        # 1 = ERROR ONLY
+        elif BIGJOB_VERBOSE == 1:
+            logging_level = logging.ERROR
+        
+      
+    #print("Set logging level: %s"%(logging_level))
+    logging.basicConfig(level=logging_level, datefmt='%m/%d/%Y %I:%M:%S %p',
+               format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(name='bigjob')
+    logger.setLevel(logging_level)
+    #logging.basicConfig(level=logging_level)        
       
     saga = default_dict["saga"]
     if saga.lower() == "bliss":
         SAGA_BLISS=True    
         
 except:
-    print("bjgjob.conf could not be read") 
+    print("bigjob.conf could not be read") 
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    traceback.print_exc(limit=1, file=sys.stdout)
     
 import socket
 try:
