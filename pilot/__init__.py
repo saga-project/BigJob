24652b75793efcc049613ac76505fcbb667c48d1
Andre Luckow <222015+drelu@users.noreply.github.com>
2012-05-31 20:37:13 +0200

    simplified pilot-api export

--- pilot/__init__.py
+++ pilot/__init__.py
@@ -1,35 +1,43 @@
 application_id = "bigjob"
 
 
-from pilot.impl.pilotcompute_manager import PilotComputeService as MyPilotComputeService
-from pilot.impl.pilotdata_manager import PilotDataService as MyPilotDataService
-from pilot.impl.pilotdata_manager import DataUnit as MyDataUnit
-from pilot.impl.pilot_manager import ComputeDataService as MyComputeDataService
-from pilot.impl.pilot_manager import ComputeUnit as MyComputeUnit
-from pilot.api import State as MyState
+from pilot.impl.pilotcompute_manager import PilotComputeService
+from pilot.impl.pilotdata_manager import PilotDataService
+from pilot.impl.pilotdata_manager import DataUnit
+from pilot.impl.pilot_manager import ComputeDataService 
+from pilot.impl.pilot_manager import ComputeUnit 
+from pilot.api import State 
+
+
+#from pilot.impl.pilotcompute_manager import PilotComputeService as MyPilotComputeService
+#from pilot.impl.pilotdata_manager import PilotDataService as MyPilotDataService
+#from pilot.impl.pilotdata_manager import DataUnit as MyDataUnit
+#from pilot.impl.pilot_manager import ComputeDataService as MyComputeDataService
+#from pilot.impl.pilot_manager import ComputeUnit as MyComputeUnit
+#from pilot.api import State as MyState
 
 # define external-facing API
 
-class PilotComputeService(MyPilotComputeService):
-    pass
+#class PilotComputeService(MyPilotComputeService):
+#    pass
 
 
-class PilotDataService(MyPilotDataService):
-    pass
-
-
-class ComputeUnit(MyComputeUnit):
-    pass
-
-class DataUnit(MyDataUnit):
-    pass
-
-
-class ComputeDataService(MyComputeDataService):
-    pass
-
-
-class State(MyState):
-    pass
+#class PilotDataService(MyPilotDataService):
+#    pass
+#
+#
+#class ComputeUnit(MyComputeUnit):
+#    pass
+#
+#class DataUnit(MyDataUnit):
+#    pass
+#
+#
+#class ComputeDataService(MyComputeDataService):
+#    pass
+#
+#
+#class State(MyState):
+#    pass
 
 
