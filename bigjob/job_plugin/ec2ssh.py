59c7e03171080be1e9810faf4554f86852f8f70a
drelu <andre.luckow@gmail.com>
2012-08-20 16:28:55 +0100

    ported at least compute part to eucalyptus

--- bigjob/job_plugin/ec2ssh.py
+++ bigjob/job_plugin/ec2ssh.py
@@ -6,23 +6,24 @@ import uuid
 import time
 
 from boto.ec2.connection import EC2Connection
+from boto.ec2.regioninfo import RegionInfo
 
 import bliss.saga as saga
+from pilot.api.api import PilotError
 
 ###############################################################################
 # EC2 General
-#EC2_SSH_PRIVATE_KEY_FILE="/Users/luckow/.ec2/id-lsu-keypair"
-EC2_SSH_PRIVATE_KEY_FILE="/Users/luckow/.ssh/id_rsa"
-EC2_INSTANCE_TYPE="t1.micro"
-EC2_PLACEMENT_GROUP=None
-EC2_SECURITY_GROUP="default"
+PLACEMENT_GROUP=None
+SECURITY_GROUP="default"
 
 # VM/Image specific configurations
-#EC2_AMI_ID="ami-c7943cae" #  ami-82fa58eb official Amazon Ubuntu 12.04 LTS (requires dev tools installation)
-EC2_AMI_ID="ami-d7f742be"
-EC2_USERNAME="ubuntu"
-#EC2_KEYNAME="lsu-keypair"
-EC2_KEYNAME="MyKey"
+# Recommendation Ubuntu > 1104
+# apt-get install gcc python-all-dev git subversion vim
+# EC2_AMI_ID="ami-c7943cae" #  ami-82fa58eb official Amazon Ubuntu 12.04 LTS (requires dev tools installation)
+# EC2_AMI_ID="ami-d7f742be"
+# EC2_USERNAME="ubuntu"
+# EC2_KEYNAME="lsu-keypair"
+# EC2_KEYNAME="MyKey"
 
 # Authentication
 # Please use ~/.boto file to configure your security credentials (if possible)
@@ -36,26 +37,31 @@ EC2_KEYNAME="MyKey"
 AWS_ACCESS_KEY_ID=None
 AWS_SECRET_ACCESS_KEY=None
 
+
+
+
 class State:
     PENDING="pending"
     RUNNING="running"
     
     
 class Service(object):
-    """ Plugin for Amazon EC2 
+    """ Plugin for Amazon EC2 and EUCA
     
         Manages endpoint in the form of:
         
-            ec2+ssh://<EC2 Endpoint>    
+            ec2+ssh://<EC2 Endpoint>
+            euca+ssh://<EUCA Endpoint>
     """
 
-    def __init__(self, resource_url):
+    def __init__(self, resource_url, pilot_compute_description=None):
         """Constructor"""
         self.resource_url = resource_url
+        self.pilot_compute_description = pilot_compute_description
             
     
     def create_job(self, job_description):
-        j = Job(job_description, self.resource_url)
+        j = Job(job_description, self.resource_url, self.pilot_compute_description)
         return j
             
     
@@ -69,29 +75,59 @@ class Job(object):
     """ Plugin for Amazon EC2 
     
         Starts VM and executes BJ agent on this VM    
+        
+        
+        Eucalyptus on FutureGrid uses a self-signed certificate, which 1) needs to be added to boto configuration
+        or 2) certificate validation needs to be disabled.
     """
 
-    def __init__(self, job_description, saga_url):
+    def __init__(self, job_description, resource_url, pilot_compute_description):
         
         self.job_description = job_description
-        self.saga_url = saga_url
+        self.resource_url = saga.Url(resource_url)
+        self.pilot_compute_description = pilot_compute_description
         
         self.id="bigjob-" + str(uuid.uuid1())
         self.network_ip=None
         
-        self.ec2_conn = EC2Connection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+        self.ec2_conn=None
+        if self.resource_url.scheme == "euca+ssh":
+            region = RegionInfo(name="eucalyptus", endpoint=self.resource_url.host)
+            self.ec2_conn = EC2Connection(aws_access_key_id=self.pilot_compute_description["access_key_id"], 
+                                          aws_secret_access_key=self.pilot_compute_description["secret_access_key"], 
+                                          region=region,
+                                          port=8773,
+                                          path="/services/Eucalyptus")
+        else:
+            self.ec2_conn = EC2Connection(aws_access_key_id=self.pilot_compute_description["access_key_id"], 
+                                          aws_secret_access_key=self.pilot_compute_description["secret_access_key"])
         self.instance = None
         
         
     def run(self):
-        reservation = self.ec2_conn.run_instances(EC2_AMI_ID,
-                                    key_name=EC2_KEYNAME,
-                                    instance_type=EC2_INSTANCE_TYPE,
-                                    security_groups=[EC2_SECURITY_GROUP])
+        """ Start VM and start BJ agent via SSH on VM """
+        
+        """ Map fields of Pilot description to EC2 API
+            { "vm_id":"ami-d7f742be",
+              "vm_ssh_username":"ubuntu",
+              "vm_ssh_keyname":"MyKey",
+              "vm_ssh_keyfile":"<path>",
+              "vm_type":"t1.micro",
+              "access_key_id":"xxx",
+              "secret_access_key":"xxx"
+            }
+        """    
+            
+        reservation = self.ec2_conn.run_instances(self.pilot_compute_description["vm_id"],
+                                    key_name=self.pilot_compute_description["vm_ssh_keyname"],
+                                    instance_type=self.pilot_compute_description["vm_type"],
+                                    security_groups=[SECURITY_GROUP])
+                
         self.instance = reservation.instances[0]
         self.instance_id = self.instance.id
         logger.debug("Started EC2 instance: %s"%self.instance_id)
-        self.ec2_conn.create_tags([self.instance_id], {"Name": self.id})
+        if self.resource_url.scheme != "euca+ssh":
+            self.ec2_conn.create_tags([self.instance_id], {"Name": self.id})
         self.wait_for_running()
         
         self.network_ip = self.instance.ip_address 
@@ -102,8 +138,8 @@ class Job(object):
         # Submit job
         ctx = saga.Context()
         ctx.type = saga.Context.SSH
-        ctx.userid = EC2_USERNAME
-        ctx.userkey = EC2_SSH_PRIVATE_KEY_FILE
+        ctx.userid = self.pilot_compute_description["vm_ssh_username"]
+        ctx.userkey = self.pilot_compute_description["vm_ssh_keyfile"]
         js.session.contexts.append(ctx)
 
         job = js.create_job(self.job_description)
--- examples/pilot-api/example-pilot-compute-data-cloud.py
+++ examples/pilot-api/example-pilot-compute-data-cloud.py
@@ -46,16 +46,57 @@ if __name__ == "__main__":
     pilot_compute_service = PilotComputeService(coordination_url=COORDINATION_URL)
 
     # create pilot job service and initiate a pilot job
-    pilot_compute_description = {
+    pilot_compute_description_amazon = {
                              "service_url": 'ec2+ssh://aws.amazon.com',
+                             #"service_url": 'euca+ssh://198.202.120.90/',
                              #"service_url": 'gce+ssh://api.google.com',
                              #"service_url": 'fork://localhost',
                              "number_of_processes": 1,                             
                              'affinity_datacenter_label': "us-google",              
-                             'affinity_machine_label': "" 
+                             'affinity_machine_label': "", 
+                             # cloud specific attributes
+                             "vm_id":"ami-d7f742be",
+                             "vm_ssh_username":"ubuntu",
+                             "vm_ssh_keyname":"MyKey",
+                             "vm_ssh_keyfile":"/Users/luckow/.ssh/id_rsa",
+                             "vm_type":"t1.micro"
+    
                             }
     
-    pilotjob = pilot_compute_service.create_pilot(pilot_compute_description=pilot_compute_description)
+    pilot_compute_description_euca_sierra = {
+                             #"service_url": 'ec2+ssh://aws.amazon.com',
+                             "service_url": 'euca+ssh://198.202.120.90/',
+                             #"service_url": 'gce+ssh://api.google.com',
+                             #"service_url": 'fork://localhost',
+                             "number_of_processes": 1,                             
+                             'affinity_datacenter_label': "us-west",              
+                             'affinity_machine_label': "", 
+                             # cloud specific attributes
+                             "vm_id":"ami-d7f742be",
+                             "vm_ssh_username":"ubuntu",
+                             "vm_ssh_keyname":"MyKey",
+                             "vm_ssh_keyfile":"/Users/luckow/.ssh/id_rsa",
+                             "vm_type":"c1.xlarge",
+                             "access_key_id":"OQTYSHQ9I6ACEXDTKTOGG",
+                             "secret_access_key":"STgYQPTyh89EWKp7lFtd77A4O2aghXjx0owqkNHm"
+                            }
+    
+    pilot_compute_description_euca_india = {
+                             "service_url": 'euca+ssh://149.165.146.135/',
+                             "number_of_processes": 1,                             
+                             'affinity_datacenter_label': "us-east",              
+                             'affinity_machine_label': "", 
+                             # cloud specific attributes
+                             "vm_id":"emi-D0C037B6",
+                             "vm_ssh_username":"root",
+                             "vm_ssh_keyname":"eucakey",
+                             "vm_ssh_keyfile":"/Users/luckow/.ssh/eucakey-india",
+                             "vm_type":"c1.xlarge",
+                             "access_key_id":"8MCXRAMXMHDYKWNKXZ8WF",
+                             "secret_access_key":"YrcUqSw2Arxshrh3ZtenkxerWwCWdMTKvZYoLPAo"
+                            }
+    
+    pilotjob = pilot_compute_service.create_pilot(pilot_compute_description=pilot_compute_description_euca_india)
     
     compute_data_service = ComputeDataService()
     compute_data_service.add_pilot_compute_service(pilot_compute_service)
@@ -64,8 +105,8 @@ if __name__ == "__main__":
     
     # start work unit
     compute_unit_description = {
-            "executable": "/bin/cat",
-            "arguments": ["test.txt"],
+            "executable": "/bin/date",
+            "arguments": [],
             "number_of_processes": 1,
             "output": "stdout.txt",
             "error": "stderr.txt",   
