9c30ab210e02c0fdd4f42e56788e774a1bc0290a
Melissa Romanus <melissa.romanus@gmail.com>
2013-02-14 15:47:27 -0500

    Twitter bootstrap

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 # serve to show the default.
 
 import sys, os
+import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -94,7 +95,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+html_theme = 'bootstrap'
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -104,7 +105,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
