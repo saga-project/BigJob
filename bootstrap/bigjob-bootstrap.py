396124286cf98a223b7a115755a418a6ae60933e
Andre Luckow <andre.luckow@gmail.com>
2014-01-23 21:48:39 -0500

    0.59.1

--- bootstrap/bigjob-bootstrap.py
+++ bootstrap/bigjob-bootstrap.py
@@ -1817,9 +1817,9 @@ def after_install(options, home_dir):
     if not os.path.exists(etc):
         os.makedirs(etc)         
     print "load distribute"
-    subprocess.call(["curl", "-O", "http://python-distribute.org/distribute_setup.py"])    
-    print "install distribute"
-    subprocess.call([join(home_dir, 'bin', 'python'), 'distribute_setup.py'])
+    subprocess.call(["curl", "-O", "https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py"])    
+    print "install setuptools"
+    subprocess.call([join(home_dir, 'bin', 'python'), 'ez_setup.py'])
     print "install bigjob"
     subprocess.call([join(home_dir, 'bin', 'easy_install'), 'bigjob'])    
 
